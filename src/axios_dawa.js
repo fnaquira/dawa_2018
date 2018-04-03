0bdb4d2acc60ba3c7ded1f3a4ac962f80452205e
Favio <fnaquira@conflux-aqp.com>
2018-04-03 12:24:18 -0500

    previo examen

--- src/axios_dawa.js
+++ src/axios_dawa.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'http://35.193.107.240:8000/',
     //headers: {'Access-Control-Allow-Origin': '*'}
 });
 
