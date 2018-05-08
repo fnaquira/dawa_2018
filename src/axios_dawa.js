59796dc7e03ab5fc1d640983e72ff72e0ddd9331
Favio Náquira <fnaquira@conflux-aqp.com>
2018-05-08 12:51:10 -0500

    nuevo bundle apuntando al server remoto

--- src/axios_dawa.js
+++ src/axios_dawa.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/',
-    //headers: {'Access-Control-Allow-Origin': '*'}
+    //baseURL: 'http://localhost:8000/',
+    baseURL: 'http://35.226.64.247:8000/',
+    headers: {'Access-Control-Allow-Origin': '*'}
 });
 
 export default instance;
