54b83f161db42cf6c765934a11f05f7cb6a77e0e
Favio Náquira <fnaquira@conflux-aqp.com>
2018-05-08 14:22:06 -0500

    se actualiza la direccion a dawa.conflux.pe

---
+++ bd/quiz02/quiz.js
@@ -0,0 +1,12 @@
+[
+{cod:'A1',title:'Prueba 1',isCorrect:'3',answers:['1','2','3','4','5']},
+{cod:'A2',title:'Prueba 2',isCorrect:'2',answers:['1','2','3','4','5']},
+{cod:'A3',title:'Prueba 3',isCorrect:'3',answers:['1','2','3','4','5']},
+{cod:'A4',title:'Prueba 4',isCorrect:'4',answers:['1','2','3','4','5']},
+{cod:'A5',title:'Prueba 5',isCorrect:'1',answers:['1','2','3','4','5']},
+{cod:'A6',title:'Prueba 6',isCorrect:'5',answers:['1','2','3','4','5']},
+{cod:'A7',title:'Prueba 7',isCorrect:'2',answers:['1','2','3','4','5']},
+{cod:'A8',title:'Prueba 8',isCorrect:'4',answers:['1','2','3','4','5']},
+{cod:'A9',title:'Prueba 9',isCorrect:'1',answers:['1','2','3','4','5']},
+{cod:'A10',title:'Prueba 10',isCorrect:'4',answers:['1','2','3','4','5']}
+]
--- src/axios_dawa.js
+++ src/axios_dawa.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: 'http://localhost:8000/',
-    baseURL: 'http://35.226.64.247:8000/',
+    baseURL: 'http://dawa.conflux.pe/',
     headers: {'Access-Control-Allow-Origin': '*'}
 });
 
